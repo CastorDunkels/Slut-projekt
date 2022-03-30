bd6f93fae6434ef62d1d31b49ce5cd646ad5634a
Castor Dunkels <castordnk@gmail.com>
2022-03-30 12:27:23 +0200

    .

--- Slut-projekt/Program.cs
+++ Slut-projekt/Program.cs
@@ -1,2 +1,15 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
+using Raylib_cs;
 
+Raylib.InitWindow(1000, 800, "Game");
+Raylib.SetTargetFPS(600);
+//sonny likt spel ska jag kanske göra
+//göra runda då man kan attackera och så fienderna kan attackera
+//göra en timer som gör så att om man inte attackerar i den tiden så skippas din runda så fienderna attackerar
+//göra att fienderna attackerar bara då spelarens tur har gått
+//göra så man kan välja mellan flera attacker som gör olika saker
+
+Raylib.BeginDrawing();
+
+Raylib.EndDrawing();
