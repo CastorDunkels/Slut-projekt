be32efa65ba61ace71bb87f40fb55a1f98b4d32b
Castor Dunkels <castordnk@gmail.com>
2022-03-30 12:55:09 +0200

    ritar ut basic shapes

--- Slut-projekt/Program.cs
+++ Slut-projekt/Program.cs
@@ -2,14 +2,35 @@
 using System;
 using Raylib_cs;
 
-Raylib.InitWindow(1000, 800, "Game");
-Raylib.SetTargetFPS(600);
 //sonny likt spel ska jag kanske göra
+//göra så man kan klicka på fienderna och sig själv
 //göra runda då man kan attackera och så fienderna kan attackera
 //göra en timer som gör så att om man inte attackerar i den tiden så skippas din runda så fienderna attackerar
 //göra att fienderna attackerar bara då spelarens tur har gått
 //göra så man kan välja mellan flera attacker som gör olika saker
 
+const int PLAYERSPAWNX = 100;
+const int PLAYERSPAWNY = 400;
+const int PLAYERWIDTH = 100;
+const int PLAYERHEIGHT = 50;
+float enemyX = 800;
+float enemyY = 400;
+float enemyWidth = 100;
+float enemyHeight = 50;
+
+Rectangle playerRect = new Rectangle(PLAYERSPAWNX, PLAYERSPAWNY, PLAYERHEIGHT, PLAYERWIDTH);
+Rectangle enemyRect = new Rectangle(enemyX, enemyY, enemyHeight, enemyWidth);
+
+Raylib.InitWindow(1000, 900, "Game");
+Raylib.SetTargetFPS(60);
+
+
+while(Raylib.WindowShouldClose() == false){
+
 Raylib.BeginDrawing();
+Raylib.ClearBackground(Color.SKYBLUE);
+Raylib.DrawRectangleRec(playerRect, Color.GREEN);
+Raylib.DrawRectangleRec(enemyRect, Color.DARKPURPLE);
 
 Raylib.EndDrawing();
+}
