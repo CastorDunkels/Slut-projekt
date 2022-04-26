e2c5701095737e4af19bbcf1b5ae4efd0e2d0f10
Castor Dunkels <castordnk@gmail.com>
2022-04-26 19:27:09 +0200

    fienden kan skada dig

--- Slut-projekt/Program.cs
+++ Slut-projekt/Program.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System.Data;
+using System.Collections.Generic;
 using System;
 using Raylib_cs;
 
-//sonny likt spel 
+//sonny/swords and sandals likt spel 
 
 //fråga om man vill möta svår eller enkel fiende
 //timer som långsamt går ut 
@@ -24,10 +25,16 @@ float playerX = 100;
 float playerY = 400;
 float enemyX = 800;
 float enemyY = 400;
+double roundTime = 0;
+float playerHealth = 100;
 
 
 Rectangle playerRect = new Rectangle(playerX, playerY, PLAYERWIDTH, PLAYERHEIGHT);
 Rectangle enemyRect = new Rectangle(enemyX, enemyY, ENEMYWIDTH, ENEMYHEIGHT);
+
+bool playerOptions = false;
+bool enemyOptions = false;
+
 
 bool playerPress()
 {
@@ -35,17 +42,13 @@ bool playerPress()
     float playerY2 = playerY + PLAYERHEIGHT;
     float mouseX = Raylib.GetMouseX();
     float mouseY = Raylib.GetMouseY();
-    Raylib.DrawText(":" + playerX, 400, 100, 30, Color.BLACK);
-    Raylib.DrawText(":" + mouseX, 400, 600, 30, Color.BLACK);
-    Raylib.DrawText(":" + playerY, 100, 100, 30, Color.BLACK);
-    Raylib.DrawText(":" + mouseY, 100, 600, 30, Color.BLACK);
-    if (Raylib.IsMouseButtonDown(0) &&
+    if (Raylib.IsMouseButtonPressed(0) &&
         playerX <= mouseX &&
         playerX2 >= mouseX &&
         playerY <= mouseY &&
         playerY2 >= mouseY)
     {
-        Raylib.DrawText("funnyhaha", 100, 100, 100, Color.BROWN);
+        playerOptions = !playerOptions;
         return true;
     }
     else
@@ -53,6 +56,55 @@ bool playerPress()
         return false;
     }
 
+}
+bool enemyPress()
+{
+    float enemyX2 = enemyX + ENEMYWIDTH;
+    float enemyY2 = enemyY + ENEMYHEIGHT;
+    float mouseX = Raylib.GetMouseX();
+    float mouseY = Raylib.GetMouseY();
+    if (Raylib.IsMouseButtonPressed(0) &&
+        enemyX <= mouseX &&
+        enemyX2 >= mouseX &&
+        enemyY <= mouseY &&
+        enemyY2 >= mouseY)
+    {
+        enemyOptions = !enemyOptions;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+void enemyAttack()
+{
+    
+    Random hitRate = new Random();
+    int enemyHitRate = hitRate.Next(1, 11);//enemy has a 80% chance to deal dmg 
+    Random rnd = new Random();
+    int dmgTaken = rnd.Next(30, 51);
+    if (enemyHitRate >= 9)
+    {
+        Raylib.DrawText("Miss", (int)playerX + 60, (int)playerY, 20, Color.DARKGRAY);
+    }
+    else
+    {
+        playerHealth -= dmgTaken;
+    }
+
+}
+
+void timer()
+{
+    if (Raylib.GetTime() - roundTime > 30)
+    {
+        enemyAttack();
+        roundTime = Raylib.GetTime();
+        enemyOptions = false;
+        playerOptions = false;
+    }
 }
 
 
@@ -62,9 +114,29 @@ while (Raylib.WindowShouldClose() == false)
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.SKYBLUE);
+    timer();
     playerPress();
+    enemyPress();
     Raylib.DrawRectangleRec(playerRect, Color.GREEN);
     Raylib.DrawRectangleRec(enemyRect, Color.DARKPURPLE);
+    Raylib.DrawText(":" + playerHealth, 100, 600, 30, Color.BLACK);
+
+    if (playerOptions == true)
+    {
+        Raylib.DrawText("funnyhaha", 100, 100, 50, Color.BROWN);
+    }
+    if (enemyOptions == true)
+    {
+        Raylib.DrawText("Jeffy", 600, 100, 50, Color.BROWN);
+        Raylib.DrawCircle((int)enemyX + 25, (int)enemyY - 23, 23, Color.BLUE);
+
+    }
+
+    if(playerHealth <=0){
+        playerHealth = 0;
+
+
+    }
 
     Raylib.EndDrawing();
 }
