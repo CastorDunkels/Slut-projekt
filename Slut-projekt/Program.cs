3b6c2872b8afbea0000b04ef3c4b5f061ad61cd8
Castor Dunkels <castordnk@gmail.com>
2022-05-03 14:55:52 +0200

    attack and heal

--- Slut-projekt/Program.cs
+++ Slut-projekt/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Data;
 using System.Collections.Generic;
 using System;
@@ -35,12 +36,26 @@ float enemyY = 400;
 double roundTime = 0;
 float playerHealth = 100;
 float enemyHealth = 1000;
-float enemyOptionX = enemyX + PLAYERWIDTH / 2;
+float playerHealX = playerX + PLAYERWIDTH / 2;
+float playerHealY = playerY - 20;
+float enemyOptionX = enemyX + ENEMYWIDTH / 2;
 float enemyOptionY = enemyY - 20;
-float enemyOption2X = enemyOptionX - PLAYERWIDTH + 5;
+float enemyOption2X = enemyOptionX - ENEMYWIDTH + 5;
 float enemyOption2Y = enemyOptionY + 40;
-float enemyOption3X = enemyOptionX + PLAYERWIDTH - 5;
+float enemyOption3X = enemyOptionX + ENEMYWIDTH - 5;
 float enemyOption3Y = enemyOptionY + 40;
+float playerHealthX = playerX;
+float playerHealthY = playerY + 100;
+float playerHealthBarOutlineX = playerX;
+float playerHealthBarOutlineY = playerY + 150;
+float playerHealthBarX = playerHealthBarOutlineX + 2;
+float playerHealthBarY = playerHealthBarOutlineY + 1;
+float enemyHealthX = enemyX;
+float enemyHealthY = enemyY + 100;
+float enemyHealthBarOutlineX = enemyX;
+float enemyHealthBarOutlineY = enemyY + 150;
+float enemyHealthBarX = enemyHealthBarOutlineX + 2;
+float enemyHealthBarY = enemyHealthBarOutlineY + 1;
 
 
 Rectangle playerRect = new Rectangle(playerX, playerY, PLAYERWIDTH, PLAYERHEIGHT);
@@ -92,6 +107,139 @@ bool enemyPress()
     }
 
 }
+bool heal(){
+    float mouseX = Raylib.GetMouseX();
+    float mouseY = Raylib.GetMouseY();
+    float healX = 0;
+    float healY= 0;
+    float healX2 = healX + 100;
+    float healY2= healY + 100;
+    if (playerOptions == true){
+
+        if (Raylib.IsMouseButtonPressed(0) &&
+            healX <= mouseX &&
+            healX2 >= mouseX &&
+            healY <= mouseY &&
+            healY2 >= mouseY)
+        {
+            playerHealth += 50;
+            roundTime -= 30;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+}
+bool weakAttack(){
+    float mouseX = Raylib.GetMouseX();
+    float mouseY = Raylib.GetMouseY();
+    float weakAttackX = enemyX;
+    float weakAttackY = 0;
+    float weakAttackX2 = weakAttackX + 100;
+    float weakAttackY2= weakAttackY + 100;
+    if (enemyOptions == true){
+
+        if (Raylib.IsMouseButtonPressed(0) &&
+            weakAttackX <= mouseX &&
+            weakAttackX2 >= mouseX &&
+            weakAttackY <= mouseY &&
+            weakAttackY2 >= mouseY)
+        {
+            enemyHealth -= 20;
+            roundTime -= 30;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+}
+bool mediumAttack(){
+    float mouseX = Raylib.GetMouseX();
+    float mouseY = Raylib.GetMouseY();
+    float medAttackX = enemyX;
+    float medAttackY = enemyY + 100;
+    float medAttackX2 = medAttackX + 100;
+    float medAttackY2= medAttackY + 100;
+    if (enemyOptions == true){
+
+        if (Raylib.IsMouseButtonPressed(0) &&
+            medAttackX <= mouseX &&
+            medAttackX2 >= mouseX &&
+            medAttackY <= mouseY &&
+            medAttackY2 >= mouseY)
+        {
+            Random hitRate = new Random();
+            int medAttackHitRate = hitRate.Next(1, 11);
+            if(medAttackHitRate >= 8){
+                Raylib.DrawText("Miss",(int)enemyX - 60, (int)enemyY, 30, Color.RED);
+            }
+            else{
+            enemyHealth -= 100;
+            Raylib.DrawText("-" + 100, (int)enemyX - 60, (int)enemyY, 30, Color.BLUE);
+
+            }
+            roundTime -= 30;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+}
+bool strongAttack(){
+    float mouseX = Raylib.GetMouseX();
+    float mouseY = Raylib.GetMouseY();
+    float strongAttackX = enemyX + 100;
+    float strongAttackY = enemyY + 100;
+    float strongAttackX2 = strongAttackX + 100;
+    float strongAttackY2= strongAttackY + 100;
+    if (enemyOptions == true){
+
+        if (Raylib.IsMouseButtonPressed(0) &&
+            strongAttackX <= mouseX &&
+            strongAttackX2 >= mouseX &&
+            strongAttackY <= mouseY &&
+            strongAttackY2 >= mouseY)
+        {
+            Random hitRate = new Random();
+            int strongAttackHitRate = hitRate.Next(1, 101);
+            if(strongAttackHitRate >= 2){
+                Raylib.DrawText("Miss",(int)enemyX - 60, (int)enemyY, 30, Color.RED);
+            }
+            else{
+            enemyHealth -= 1000;
+            Raylib.DrawText("-" + 1000, (int)enemyX - 60, (int)enemyY, 30, Color.BLUE);
+
+            }
+            roundTime -= 30;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+}
+void enemyHeal(){
+    float heals = 0;
+    if(enemyHealth <= 200 && heals == 0){
+        enemyHealth += 1000;
+        heals += 1;
+    }
+}
 void enemyAttack()
 {
 
@@ -113,7 +261,7 @@ void enemyAttack()
 
 void timer()
 {
-    if (Raylib.GetTime() - roundTime > 5)
+    if (Raylib.GetTime() - roundTime > 30)
     {
         enemyAttack();
         roundTime = Raylib.GetTime();
@@ -122,23 +270,7 @@ void timer()
     }
 }
 
-/*bool attackChoice() {
-    float mouseX = Raylib.GetMouseX();
-    float mouseY = Raylib.GetMouseY();
-    float dX = mouseX - enemyOptionX;
-    float dY = mouseY - enemyOptionY;
-    if(Raylib.IsMouseButtonPressed(0) && ){
-        return true;
-        Raylib.DrawText("JEFF", 500, 400, 40, Color.BLACK);
-    }
-    else{
-        return false;
-    }
-}*/
 
-//bool heal() {
-
-//}
 
 
 while (Raylib.WindowShouldClose() == false)
@@ -150,25 +282,28 @@ while (Raylib.WindowShouldClose() == false)
     timer();
     playerPress();
     enemyPress();
-    //attackChoice();
+    heal();
+    weakAttack();
+    mediumAttack();
+    strongAttack();
+    enemyHeal();
     Raylib.DrawRectangleRec(playerRect, Color.GREEN);
     Raylib.DrawRectangleRec(enemyRect, Color.DARKPURPLE);
-    Raylib.DrawText(":" + playerHealth, (int)playerX, (int)playerY + 100, 30, Color.BLACK);
+    Raylib.DrawText(":" + playerHealth, (int)playerHealthX, (int)playerHealthY, 30, Color.BLACK);
+    Raylib.DrawText(":" + enemyHealth, (int)enemyHealthX, (int)enemyHealthY, 30, Color.BLACK);
     Raylib.DrawText("Time: " + ((int)Raylib.GetTime() - (int)roundTime), 450, 600, 30, Color.BLACK);
-    Raylib.DrawRectangle((int)playerX, (int)playerY + 150, 100, 20, Color.BLACK);
-    Raylib.DrawRectangle((int)playerX + 2, (int)playerY + 151, 96, 18, Color.WHITE);
-    Raylib.DrawRectangle((int)playerX + 2, (int)playerY + 151, (int)playerHealth * 96 / 100, 18, Color.RED);
-    Raylib.DrawRectangle((int)enemyX, (int)enemyY + 150, 100, 20, Color.BLACK);
-    Raylib.DrawRectangle((int)enemyX + 2, (int)enemyY + 151, 96, 18, Color.BLACK);
-    Raylib.DrawRectangle((int)enemyX + 2, (int)enemyY + 151, (int)enemyHealth * 96 / 1000, 18, Color.RED);
+    Raylib.DrawRectangle((int)playerHealthBarOutlineX, (int)playerHealthBarOutlineY, 100, 20, Color.BLACK);
+    Raylib.DrawRectangle((int)playerHealthBarX, (int)playerHealthBarY, 96, 18, Color.WHITE);
+    Raylib.DrawRectangle((int)playerHealthBarX, (int)playerHealthBarY, (int)playerHealth * 96 / 100, 18, Color.RED);
+    Raylib.DrawRectangle((int)enemyHealthBarOutlineX, (int)enemyHealthBarOutlineY, 100, 20, Color.BLACK);
+    Raylib.DrawRectangle((int)enemyHealthBarX, (int)enemyHealthBarY, 96, 18, Color.BLACK);
+    Raylib.DrawRectangle((int)enemyHealthBarX, (int)enemyHealthBarY, (int)enemyHealth * 96 / 1000, 18, Color.RED);
 
     if (playerOptions == true)
     {
         Raylib.DrawText("funnyhaha", 100, 100, 50, Color.BROWN);
+        Raylib.DrawCircle((int)playerHealX, (int)playerHealY, 18, Color.BLUE);
     }
-    /*while (playerOptions == true){
-        heal();
-    }*/
 
     if (enemyOptions == true)
     {
@@ -177,15 +312,21 @@ while (Raylib.WindowShouldClose() == false)
         Raylib.DrawCircle((int)enemyOption2X, (int)enemyOption2Y, 18, Color.BLUE);
         Raylib.DrawCircle((int)enemyOption3X, (int)enemyOption3Y, 18, Color.BLUE);
     }
-    /*while (enemyOptions == true){
-        attackChoice();
-    }*/
 
     if (playerHealth <= 0)
     {
         playerHealth = 0;
 
 
+    }
+    if (playerHealth >= 101){
+        playerHealth = 100;
+    }
+    if (enemyHealth <=0){
+        enemyHealth = 0;
+    }
+    if (enemyHealth >= 1001){
+        enemyHealth = 1000;
     }
 
     Raylib.EndDrawing();
